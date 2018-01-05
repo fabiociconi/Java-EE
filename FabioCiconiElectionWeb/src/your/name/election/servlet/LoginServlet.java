5b2706b85f95eda7fc075ce44517895e88180723
ZeusAC <fabiociconi@yahoo.com.br>
2018-01-05 15:12:04 -0200

    finale assignment

--- FabioCiconiElectionWeb/src/your/name/election/servlet/LoginServlet.java
+++ FabioCiconiElectionWeb/src/your/name/election/servlet/LoginServlet.java
@@ -3,6 +3,7 @@ package your.name.election.servlet;
 import java.io.IOException;
 import java.util.List;
 
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,11 +11,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import your.name.election.model.CandidateManager;
-import your.name.election.model.ElectionManager;
+import your.name.election.ejbs.CandidateManager;
+import your.name.election.ejbs.CandidateManagerLocal;
+import your.name.election.ejbs.ElectionManager;
+import your.name.election.ejbs.ElectionManagerLocal;
 import your.name.entities.Candidate;
 import your.name.entities.Voter;
-import your.name.exceptions.ElectionException;
+import your.name.execptions.ElectionException.ElectionException;
 
 /**
  * Servlet implementation class LoginServlet
@@ -22,7 +25,10 @@ import your.name.exceptions.ElectionException;
 @WebServlet("/signin")
 public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
-
+    @EJB
+    CandidateManagerLocal cm;
+    @EJB
+    ElectionManagerLocal electionManager;
     /**
      * @see HttpServlet#HttpServlet()
      */
--- FabioCiconiElectionWeb/src/your/name/election/servlet/VoteServlet.java
+++ FabioCiconiElectionWeb/src/your/name/election/servlet/VoteServlet.java
@@ -3,6 +3,7 @@ package your.name.election.servlet;
 import java.io.IOException;
 import java.util.List;
 
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,11 +11,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import your.name.election.model.ElectionManager;
+import your.name.election.ejbs.CandidateManagerLocal;
+import your.name.election.ejbs.ElectionManager;
+import your.name.election.ejbs.ElectionManagerLocal;
 import your.name.entities.Candidate;
 import your.name.entities.Voter;
 import your.name.exceptions.DataInputException;
-import your.name.exceptions.ElectionException;
+import your.name.execptions.ElectionException.ElectionException;
+
 
 /**
  * Servlet implementation class VoteServlet
@@ -23,6 +27,8 @@ import your.name.exceptions.ElectionException;
 public class VoteServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
+    @EJB
+    ElectionManagerLocal electionManager;
     /**
      * @see HttpServlet#HttpServlet()
      */
