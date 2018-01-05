049a1fe79906e2d30b5f83a0a4fcd78d8d5a486e
ZeusAC <fabiociconi@yahoo.com.br>
2018-01-05 16:13:51 -0200

    qwe

--- FabioCiconiElectionWeb/src/your/name/election/servlet/VoteServlet.java
+++ FabioCiconiElectionWeb/src/your/name/election/servlet/VoteServlet.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import your.name.election.ejbs.CandidateManagerLocal;
 import your.name.election.ejbs.ElectionManager;
 import your.name.election.ejbs.ElectionManagerLocal;
 import your.name.entities.Candidate;
